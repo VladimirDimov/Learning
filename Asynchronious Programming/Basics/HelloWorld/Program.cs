8798428798bf679f5f3c7324ffdc7ac26543409a
VladimirDimov <vdimov1124@gmail.com>
2016-04-09 06:00:28 +0300

    Hello world added

---
+++ Asynchronious Programming/Basics/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            var task = new Task(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Hello world");
+            });
+            task.Start();
+            Console.WriteLine("Waiting");
+            task.Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
