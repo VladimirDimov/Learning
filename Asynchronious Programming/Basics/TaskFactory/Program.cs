322a6e7bb4e0910e79c206aac08d4f530ee4f69a
VladimirDimov <vdimov1124@gmail.com>
2016-04-09 06:52:05 +0300

    Task factory and task.run added

---
+++ Asynchronious Programming/Basics/TaskFactory/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskFactory
+{
+    class Program
+    {
+        static void Main()
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Hello world");
+            });
+
+            Console.WriteLine("Waiting ...");
+            task.Wait();
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Asynchronious Programming/Basics/TaskRun/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskRun
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var task = Task.Run(new Action(SayHello));
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void SayHello()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Hello");
+        }
+    }
+}
