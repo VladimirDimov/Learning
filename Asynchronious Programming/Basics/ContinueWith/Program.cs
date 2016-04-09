a6d17183bd66a3601a5d4f730b4430cd933a7edd
VladimirDimov <vdimov1124@gmail.com>
2016-04-09 16:51:29 +0300

    ContinueWith added

---
+++ Asynchronious Programming/Basics/ContinueWith/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ContinueWith
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+            var firstTask = Task.Factory.StartNew<string>(() =>
+            {
+                Console.WriteLine("Enter some text:");
+                var result = Console.ReadLine();
+                return result;
+            });
+
+            var secondTask = firstTask.ContinueWith((ft) =>
+            {
+                Console.WriteLine($"You entered: {ft.Result}");
+            });
+
+            secondTask.Wait();
+        }
+    }
+}
