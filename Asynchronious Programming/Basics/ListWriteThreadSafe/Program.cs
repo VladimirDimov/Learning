339de3039df383ea8c8a79090ef20c4923e8d60b
VladimirDimov <vdimov1124@gmail.com>
2016-04-15 22:58:56 +0300

    Thread safe write to list added

---
+++ Asynchronious Programming/Basics/ListWriteThreadSafe/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ListWriteThreadSafe
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+            var tasks = new List<Task>();
+            var list = new ThreadSafeList<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int j = 0; j < 10000; j++)
+                    {
+                        list.Add(j);
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine(list.Count);
+        }
+    }
+
+    public class ThreadSafeList<T>
+    {
+        private object _locker = new object();
+        private List<T> list = new List<T>();
+
+        public void Add(T item)
+        {
+            lock (_locker)
+            {
+                this.list.Add(item);
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return this.list.Count;
+            }
+        }
+    }
+}
