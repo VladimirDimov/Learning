0559cef6cf96c97675d5efb3e5827d82b7faf5b9
VladimirDimov <vdimov1124@gmail.com>
2016-04-24 15:06:11 +0300

    StoringTempData proj and MailClient.csproj added

---
+++ StoringTempData/StoringTempData/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoringTempData.Controllers
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var people = new Person[]
+            {
+                new Person {Name="Name 1", Age=20 },
+                new Person {Name="Name 2", Age=20 },
+                new Person {Name="Name 3", Age=20 },
+                new Person {Name="Name 4", Age=20 },
+                new Person {Name="Name 5", Age=20 },
+                new Person {Name="Name 6", Age=20 },
+            };
+
+            this.TempData["People"] = people;
+
+            return View();
+        }
+
+        public ActionResult SavePeople()
+        {
+            var people = this.TempData["People"];
+
+            return new HttpStatusCodeResult(200);
+        }
+    }
+}
