97f0fa054a4896d912b748a18613f282ad7aae07
VladimirDimov <vdimov1124@gmail.com>
2016-05-02 23:41:04 +0300

    practicing reactjs

---
+++ ReactJs/HelloWorld/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header.jsx'
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+         	<Header/>
+            Hello World!!!
+         </div>
+      );
+   }
+}
+
+export default App;
