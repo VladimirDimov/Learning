73ea47c5ebd8f0f873121e90412dc45cabf0d058
VladimirDimov <vdimov1124@gmail.com>
2016-05-04 22:00:51 +0300

    GetSetPropertyByName.csproj

---
+++ GetSetPropertyByName/GetSetPropertyByName/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace GetSetPropertyByName
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        public class Person
+        {
+            public Name Name { get; set; }
+
+            public int Age { get; set; }
+        }
+
+        public class Name
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+        }
+
+        static void Main()
+        {
+            var person = new Person { Name = new Name { FirstName = "Pesho", LastName = "Petrov" }, Age = 20 };
+            var propertyInfo = person.GetType().GetProperties();
+
+            foreach (var prop in propertyInfo)
+            {
+                if (!prop.PropertyType.IsClass)
+                {
+                    Console.WriteLine($"{prop.Name} : {prop.GetValue(person)}");
+                }
+            }
+        }
+    }
+}
---
+++ GetSetPropertyByName/ObjectManager/ObjectManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectManager
+{
+    public class ObjectManager<T>
+    {
+        private T obj;
+
+        public ObjectManager(T obj)
+        {
+            this.obj = obj;
+        }
+
+        public IDictionary<string, string> GetPropertiesMap()
+        {
+            var propertiesMap = new Dictionary<string, string>();
+            var propertyInfo = obj.GetType().GetProperties();
+        }
+    }
+}
