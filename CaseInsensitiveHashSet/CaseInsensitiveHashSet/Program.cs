44dd20e6e2c95ce2305cc7a8e5abe840d66196c3
VladimirDimov <vdimov1124@gmail.com>
2016-06-01 22:17:37 +0300

    case insensitive hashset added

---
+++ CaseInsensitiveHashSet/CaseInsensitiveHashSet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseInsensitiveHashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashSet = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+            hashSet.Add("pesho");
+            hashSet.Add("Pesho");
+            hashSet.Add("peSho");
+            hashSet.Add("peshO");
+
+            Console.WriteLine(hashSet.Contains("PESHO")); // index = true;
+            Console.WriteLine(hashSet.Count); // returns 1
+        }
+    }
+}
