e2fb1bccf3eecb6093b5a37cd1bb7ffc75979612
VladimirDimov <vdimov1124@gmail.com>
2016-10-26 16:59:33 +0300

    login template added

--- Angular2/Angular2WithAuthorization/Client/app/app.component.js
+++ Angular2/Angular2WithAuthorization/Client/app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './courses.component'], function(exports_1, context_1) {
+System.register(['angular2/core'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,15 +10,12 @@ System.register(['angular2/core', './courses.component'], function(exports_1, co
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1;
+    var core_1;
     var AppComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (courses_component_1_1) {
-                courses_component_1 = courses_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
@@ -27,8 +24,8 @@ System.register(['angular2/core', './courses.component'], function(exports_1, co
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<h1>My First Angular 2 App</h1><courses></courses>',
-                        directives: [courses_component_1.CoursesComponent]
+                        templateUrl: '/app/app.html',
+                        directives: []
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
