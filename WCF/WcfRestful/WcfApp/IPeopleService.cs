04493c433cb805306cdd0a9c4cb44759666ad91f
vladko_sz <vdimov1124@gmail.com>
2018-01-10 14:08:42 +0200

    sync

---
+++ WCF/WcfRestful/WcfApp/IPeopleService.cs
@@ -0,0 +1,31 @@
+﻿namespace WcfApp
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using System.ServiceModel;
+    using System.ServiceModel.Web;
+
+    [ServiceContract]
+    public interface IPeopleService
+    {
+        [OperationContract]
+        //[WebInvoke(
+        //    Method = "Get",
+        //    ResponseFormat = WebMessageFormat.Json,
+        //    BodyStyle = WebMessageBodyStyle.Wrapped,
+        //    UriTemplate = "api/{id}")]
+        [WebGet]
+        IEnumerable<PersonModel> Get(int id);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class PersonModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int Age { get; set; }
+    }
+}
---
+++ WCF/WcfRestful/WcfApp/PeopleService.svc.cs
@@ -0,0 +1,18 @@
+﻿namespace WcfApp
+{
+    using System.Collections.Generic;
+
+    public class PeopleService : IPeopleService
+    {
+        private static IEnumerable<PersonModel> people = new List<PersonModel>
+        {
+            new PersonModel { Name = "Pesho", Age = 100 },
+            new PersonModel { Name = "Misho", Age = 80 },
+        };
+
+        public IEnumerable<PersonModel> Get(int id)
+        {
+            return people;
+        }
+    }
+}
