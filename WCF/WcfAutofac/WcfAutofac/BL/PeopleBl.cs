ab17b8622a75ec4c35f6de8ce7d78041e08b600b
vladko_sz <vdimov1124@gmail.com>
2018-01-11 10:42:44 +0200

    sync

---
+++ WCF/WcfAutofac/WcfAutofac/BL/PeopleBl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfAutofac.BL
+{
+    public class PeopleBl
+    {
+        public string FormatValue(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
