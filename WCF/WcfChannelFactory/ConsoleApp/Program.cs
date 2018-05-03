48f864414ef1da26da9ba73a706a28a27d5de460
vladko_sz <vdimov1124@gmail.com>
2018-05-03 13:42:26 +0300

    sync

---
+++ WCF/WcfChannelFactory/ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using Contracts;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var factory = new System.ServiceModel.ChannelFactory<IService1>("*");
+            var channel = factory.CreateChannel();
+            var type = channel.GetType();
+            var response = channel.GetData(123);
+
+            Console.WriteLine(response);
+        }
+    }
+}
