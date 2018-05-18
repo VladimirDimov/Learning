29558c4382d0d2e3b0f6b55896fb121ae3503468
VladimirDimov <vdimov1124@gmail.com>
2018-05-18 17:47:53 +0300

    sync

---
+++ Aspose/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Pdf;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // load HTML contents<o:p></o:p>
+
+            using (var stream = new MemoryStream(Encoding.ASCII.GetBytes("<p>test</p>")))
+            {
+                Document source = new Document(stream, new HtmlLoadOptions());
+                source.Save("result.pdf");
+            }
+
+
+            // save output as PDF format
+        }
+    }
+}
---
+++ AsposeTest/Web/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Aspose.Pdf;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetPdf()
+        {
+            using (var sw = new StringWriter())
+            {
+                PartialViewResult result = RenderPdfTemplate();
+                result.ViewData.Model = "dfokndsafondsafokdsanf";
+                result.View = ViewEngines.Engines.FindPartialView(ControllerContext, "PdfTemplate").View;
+
+                ViewContext vc = new ViewContext(ControllerContext, result.View, result.ViewData, result.TempData, sw);
+
+                result.View.Render(vc, sw);
+
+                var html = sw.GetStringBuilder().ToString();
+
+                return this.Json(html, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public PartialViewResult RenderPdfTemplate()
+        {
+            return this.PartialView("PdfTemplate");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
