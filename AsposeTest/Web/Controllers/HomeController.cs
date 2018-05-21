a6a1a1626611ad04a4ecc12a1d31178bd9f5ef12
VladimirDimov <vdimov1124@gmail.com>
2018-05-21 14:09:24 +0300

    sync

--- AsposeTest/Web/Controllers/HomeController.cs
+++ AsposeTest/Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Aspose.Pdf;
@@ -17,25 +18,36 @@ namespace Web.Controllers
 
         public ActionResult GetPdf()
         {
-            using (var sw = new StringWriter())
+            var html = this.RenderView(this.ControllerContext, "PdfTemplate");
+            var bytes = this.PdfBytes(html);
+            var fileName = this.GeneratePdfFileName();
+            return this.File(bytes, "application/pdf", fileName);
+        }
+
+        private string GeneratePdfFileName()
+        {
+            return $"app-form.pdf";
+        }
+
+        private byte[] PdfBytes(string html)
+        {
+            using (var stream = new MemoryStream(Encoding.ASCII.GetBytes(html)))
             {
-                PartialViewResult result = RenderPdfTemplate();
-                result.ViewData.Model = "dfokndsafondsafokdsanf";
-                result.View = ViewEngines.Engines.FindPartialView(ControllerContext, "PdfTemplate").View;
+                Document source = new Document(stream, new HtmlLoadOptions());
+                using (var outputStream = new MemoryStream())
+                {
+                    source.Save(outputStream, SaveFormat.Pdf);
+                    var bytes = outputStream.ToArray();
 
-                ViewContext vc = new ViewContext(ControllerContext, result.View, result.ViewData, result.TempData, sw);
-
-                result.View.Render(vc, sw);
-
-                var html = sw.GetStringBuilder().ToString();
-
-                return this.Json(html, JsonRequestBehavior.AllowGet);
+                    return bytes;
+                }
             }
         }
 
+        [HttpGet]
         public PartialViewResult RenderPdfTemplate()
         {
-            return this.PartialView("PdfTemplate");
+            return this.PartialView("PdfTemplate", "This is from print button.");
         }
 
         public ActionResult About()
@@ -50,6 +62,21 @@ namespace Web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private string RenderView(ControllerContext controllerContext, string viewName)
+        {
+            PartialViewResult result = new PartialViewResult();
+            result.ViewData.Model = "dfokndsafondsafokdsanf";
+            result.View = ViewEngines.Engines.FindPartialView(controllerContext, viewName).View;
+            using (var sw = new StringWriter())
+            {
+                ViewContext vc = new ViewContext(controllerContext, result.View, result.ViewData, result.TempData, sw);
+                result.View.Render(vc, sw);
+                var html = sw.GetStringBuilder().ToString();
+
+                return html;
+            }
         }
     }
 }
